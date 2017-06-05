9cb3bac839e66373a0130bb9e212eae04d17da52
Marco Carraro <carraromarco89@gmail.com>
2017-06-05 16:38:06 -0700

    Working version

--- scripts/listener.py
+++ scripts/listener.py
@@ -1,11 +1,19 @@
+#!/usr/bin/env python
+
 import rospy
 from std_msgs.msg import Empty
 
 def callback(data):
-    print("I heard ",data)
+  print("I heard ")
     
 def listener():
-    rospy.init_node('save_topic')
-    trigger_topic = rospy.get_param('/trigger_topic_name')
-    rospy.Subscriber(trigger_topic, Empty, callback)
-    rospy.spin()
+  rospy.init_node('save_topic')
+  trigger_topic = rospy.get_param("/trigger_topic_name", "/trigger")
+  rospy.Subscriber(trigger_topic, Empty, callback)
+  rospy.spin()
+
+if __name__ == '__main__':
+  try:
+    listener()
+  except rospy.ROSInterruptException:
+    pass
